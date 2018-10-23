0baace811aa191d878f76d7c2dd28b8203fc827f
Shacklz <42877835+Shacklz@users.noreply.github.com>
2018-10-23 21:19:33 +0200

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('yalms-root h1')).getText();
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yalms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'yalms-desktop';
+}
